8084737428171ea0de0350e88d018379c8a0ee45
ellataira <ejtaira177@gmail.com>
2023-03-10 14:11:27 -0600

    custom pqueue to allow (score, frontierobj)

---
+++ PriorityQueue.py
@@ -0,0 +1,22 @@
+import queue
+
+
+class PriorityQueue:
+    def __init__(self):
+        self.queue = queue.PriorityQueue() # the queue only holds (score, url)
+        self.frontier_obj_dict = {} # retains all frontier_object info (not just url)
+
+    def enqueue(self, score, frontier_obj):
+        self.frontier_obj_dict[frontier_obj.link] = frontier_obj
+        t = (score, frontier_obj.link)
+        self.queue.put(t)
+
+    def get(self):
+        score, next_url = self.queue.get()
+        return score, self.frontier_obj_dict[next_url]
+
+    def get_frontier_object(self, url):
+        return self.frontier_obj_dict[url]
+
+    def get_p_queue(self):
+        return self.queue.queue
